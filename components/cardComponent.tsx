f4772a1270e33523ce31b89b64963b427b28034d
Hitigome <kawibrian1@gmail.com>
2024-02-22 05:22:59 -0800

    search event functionality

--- components/cardComponent.tsx
+++ components/cardComponent.tsx
@@ -1,11 +1,20 @@
-// src/pages/EventsPage.tsx
 'use client'
 import React, { useState, useEffect } from 'react';
+import SearchBar from './searchBar';
 import { getEvent } from '../src/ticketmasterAPI';
 
-const EventsPage: React.FC = () => {
+interface ConcertEvent {
+  id: string;
+  name: string;
+  // Add other event details as needed
+}
+
+interface EventsPageProps {
+    city: string;
+    }
+
+const EventsPage: React.FC<EventsPageProps> = ({ city }) => {
   const [events, setEvents] = React.useState<ConcertEvent[]>([]);
-  const city = 'Seattle'; 
 
   React.useEffect(() => {
     const fetchData = async () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+'use client'
 import Image from "next/image";
+import React from "react";
+import CardComponent from "../../components/cardComponent";
+import SearchBar from "../../components/searchBar";
 
 export default function Home() {
+  const [city, setCity] = React.useState<string>('');
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <header className={"bg-gray-500 h-12 flex items-center justify-center"}>
+        <SearchBar onSearch={setCity} />
+      </header>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <CardComponent city={city} />
     </div>
   );
 }
