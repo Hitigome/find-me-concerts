2cb98b1e7c9160918329b7472ff4a8a2af42fbaf
Hitigome <kawibrian1@gmail.com>
2024-02-23 02:14:35 -0800

    add images

--- src/ticketmasterAPI.ts
+++ src/ticketmasterAPI.ts
@@ -1,8 +1,13 @@
 import axios from 'axios'
 
+interface Image {
+    url: string;
+}
+
 interface ConcertEvent {
     id: string;
     name: string;
+    images: Image[];
 }
 
 interface TicketmasterResponse {
@@ -26,5 +31,19 @@ export async function getEvent(city: string): Promise<ConcertEvent[]> {
     } catch (error) {
         console.error('Failed to fetch events', error);
         return [];
+    }
+}
+
+export async function getEventImage(id: string): Promise<string> {
+    try {
+        const response = await axios.get<ConcertEvent>(`${BASE_URL}/events/${id}/images.json`, {
+            params: {
+                apikey: API_KEY,
+            },
+        });
+        return response.data.images[0].url;
+    } catch (error) {
+        console.error('Failed to fetch event images', error);
+        return '';
     }
 }
